9c0b0ca1fc6babf8885910e94311681038be8963
Mr Speaker <mrspeaker@gmail.com>
2013-08-14 08:54:32 +0200

    start of an editor for snippets

---
+++ ex/snippets/editor.js
@@ -0,0 +1,64 @@
+var editor = {
+
+	init: function () {
+
+		var code = document.querySelector("#code"),
+			ed = document.createElement("div"),
+			pre = document.createElement("pre"),
+			err = document.createElement("div"),
+			downTimer = null;
+
+		pre.innerText = code.innerText;
+
+		ed.addEventListener("keyup", function (e) {
+
+			if([37,38,39,40, 91,93, 16,17,18].indexOf(e.keyCode) >= 0) {
+				return;
+			}
+
+			clearTimeout(downTimer);
+			downTimer = setTimeout(function () {
+
+				var updatedCode = pre.innerText;
+
+				try {
+					err.innerText = "";
+					eval(updatedCode);
+					window.MainScreen = MainScreen;
+					game.init();
+				} catch (e) {
+					err.innerText = e.message;
+				}
+
+			}, 500);
+
+		}, false);
+
+		pre.style.font = "8pt monospace";
+		err.style.font = "8pt monospace";
+		err.style.position = "absolute";
+		err.style.color = "#800";
+
+		ed.setAttribute("contenteditable", true);
+		ed.style.position = "absolute";
+		ed.style.bottom = "0";
+		ed.style.left = "0";
+		ed.style.right = "0";
+		ed.style.borderTop = "1px solid #333";
+		ed.style.background = "rgba(0,0,0,0.1)";
+		ed.style.height = "300px";
+		ed.style.overflow = "auto";
+
+		ed.appendChild(err);
+		ed.appendChild(pre);
+		document.body.appendChild(ed);
+
+	}
+
+};
+
+window.addEventListener("load", function () {
+
+	editor.init();
+
+}, false);
