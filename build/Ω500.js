28ad524e7bf88286a43014e8a4f508bf388ddee6
Mr Speaker <mrspeaker@gmail.com>
2013-08-20 20:48:58 +0200

    seperate state helper, fix examples

--- build/Ω500.js
+++ build/Ω500.js
@@ -340,6 +340,68 @@ window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequ
 	};
 
 }(Ω));
+(function(Ω) {
+
+	"use strict";
+
+	var State = function (state) {
+
+		this.state = state;
+		this.last = "";
+		this.count = -1;
+		this.locked = false;
+
+	};
+
+	State.prototype = {
+
+		set: function (state) {
+
+			if (this.locked) {
+				return;
+			}
+
+			this.last = this.state;
+			this.state = state;
+			this.count = -1;
+
+		},
+
+		get: function () { return this.state; },
+
+		tick: function () { this.count++; },
+
+		first: function () { return this.count === 0; },
+
+		is: function (state) { return state === this.state; },
+
+		isNot: function (state) { return !this.is(state); },
+
+		isIn: function () {
+
+			var state = this.state,
+				args = Array.prototype.slice.call(arguments);
+
+			return args.some(function (s) {
+
+				return s === state;
+
+			});
+
+		},
+
+		isNotIn: function () {
+
+			return !(this.isIn.apply(this, arguments));
+
+		}
+
+	};
+
+	Ω.utils = Ω.utils || {};
+	Ω.utils.State = State;
+
+}(Ω));
 (function (Ω) {
 
 	"use strict";
@@ -618,60 +680,6 @@ window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequ
 			}
 
 		},
-
-	};
-
-	Ω.utils.State = function (state) {
-
-		this.state = state;
-		this.last = "";
-		this.count = -1;
-		this.locked = false;
-
-	};
-
-	Ω.utils.State.prototype = {
-
-		set: function (state) {
-
-			if (this.locked) {
-				return;
-			}
-
-			this.last = this.state;
-			this.state = state;
-			this.count = -1;
-
-		},
-
-		get: function () { return this.state; },
-
-		tick: function () { this.count++; },
-
-		first: function () { return this.count === 0; },
-
-		is: function (state) { return state === this.state; },
-
-		isNot: function (state) { return !this.is(state); },
-
-		isIn: function () {
-
-			var state = this.state,
-				args = Array.prototype.slice.call(arguments);
-
-			return args.some(function (s) {
-
-				return s === state;
-
-			});
-
-		},
-
-		isNotIn: function () {
-
-			return !(this.isIn.apply(this, arguments));
-
-		}
 
 	};
 
